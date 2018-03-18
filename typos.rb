a6ac0f676e511c5a2ae8e361efb0380aa4f371ed
aseelm <aseelmustafa@outlook.com>
2018-03-18 23:24:12 +0000

    added interactive menu to typos.rb file

--- typos.rb
+++ typos.rb
@@ -1,19 +1,43 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit 
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # % create an empty array
+  # create an empty array
   students = []
-  # % get the first name
+  # get the first name
   name = gets.chomp
-  # % while the name is not empty, repeat this code
+  # while the name is not empty, repeat this code
   while !name.empty? do
-  # % add the student hash to the array
+  # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-  # % get another name from the user
+  # get another name from the user
     name = gets.chomp
   end
-  # % return the array of students
+  # return the array of students
   if students.length == 0
     puts "there are no students"
     exit
@@ -38,7 +62,8 @@ def print_footer(names)
 end
 
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+# students = input_students
+# print_header
+# print(students)
+# print_footer(students)
+interactive_menu
