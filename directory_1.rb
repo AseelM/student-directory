5456a3d72e9d93734d171caa66a62833757c5c52
aseelm <aseelmustafa@outlook.com>
2018-03-18 23:31:44 +0000

    copied typo content to new file directory_1.rb

---
+++ directory_1.rb
@@ -0,0 +1,69 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+  # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+  # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  if students.length == 0
+    puts "there are no students"
+    exit
+  else
+    students
+  end
+end
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+
+# students = input_students
+# print_header
+# print(students)
+# print_footer(students)
+interactive_menu
